e3da8a7eaf69465346729a18090912aa579660e8
JamalE101 <jamy101@live.co.uk>
2022-03-21 10:14:24 +0000

    commit

--- src/main/java/com/example/demo/HealthApplication.java
+++ src/main/java/com/example/demo/HealthApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class DemoApplication {
+public class HealthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(HealthApplication.class, args);
 	}
 
 }
